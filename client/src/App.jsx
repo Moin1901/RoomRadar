b310b2df750c941760ff19d1fed2a1e9b39d2725
Moin1901 <mdmoinuddin1901@gmail.com>
2023-12-03 15:58:27 +0530

    etit Signin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
